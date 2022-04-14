1804370dad7db4a09dd9e8e6cccfc5ac26592004
skedaddl3 <hexisdylan23@gmail.com>
2022-04-15 00:33:11 +0800

    test commit

---
+++ lib/src/app.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:core';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+
+    final response = await get(
+        Uri.parse('https://jsonplaceholder.typicode.com/photos/$counter'));
+
+    final imageModel = ImageModel.fromJson(json.decode(response.body));
+
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images'),
+        ),
+      ),
+    );
+  }
+}
